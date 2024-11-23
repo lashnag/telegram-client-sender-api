6398dd942e5f22e96a81a89a656bb4a3d8ade0cd
a.lashnev <lexey2006.90@mail.ru>
2024-11-24 00:17:03 +0300

    Fix save session

--- app/save_session.py
+++ app/save_session.py
@@ -1,14 +1,15 @@
 from telethon import TelegramClient
-from environments_loader import load_credentials
 from telethon.sessions import StringSession
 
-api_id, api_hash, phone_number = load_credentials()
+api_id = int(input("Введите api_id: "))
+api_hash = input("Введите api_hash: ")
+phone_number = input("Введите phone_number в формате +7: ")
 
 session_string = StringSession()
 
 client = TelegramClient(session_string, api_id, api_hash)
 
-client.start()
+client.start(phone_number)
 session_str = client.session.save()
 print("Строка сессии:", session_str)
 
