127470522e959eda235e23f7d10727c024a30b37
a.lashnev <lexey2006.90@mail.ru>
2025-01-31 21:15:17 +0300

    Run server with workers

--- app/main_v2.py
+++ app/main_v2.py
@@ -6,4 +6,4 @@ from logger import init_logger
 init_logger()
 logging.getLogger().info("Main v2 run")
 
-uvicorn.run(server, host='0.0.0.0', port=4322)
+uvicorn.run(server, host='0.0.0.0', port=4322, workers=4)
